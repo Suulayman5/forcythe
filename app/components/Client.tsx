f4b258c7736788dcaecc1eec2ed8b2007b5f74dd
Mmmustapha42 <111079800+Mmmustapha42@users.noreply.github.com>
2025-01-11 22:22:19 +0100

    client animate

--- app/components/Client.tsx
+++ app/components/Client.tsx
@@ -1,8 +1,29 @@
+import Image from "next/image"
 
 const Client = () => {
   return (
     <div>
-        <div className="max-w-[56rem]"></div>
+        <div className=" flex justify-center items-center mt-8 md:mt-16 mx-4 md:mx-24 min-h-[60px] mb-8 ">
+            <h2 className="text-[2.5rem] ">Success in <span className="text-primary">Motion</span> -  our Client&apos;s Journey</h2>
+        </div>
+        <div className="flex flex-col relative overflow-hidden gap-5 bg-gradient-to-b from-transparent via-[rgba(3,5,22,0.6)] to-[rgb(3,5,22)]">
+        <div className="flex animate-marquee">
+            <Image src='https://forcythe.com/images/Project%20Images/stac.svg' width={400} height={300} alt="image"/>
+            <Image src='https://forcythe.com/images/Project%20Images/activity.svg' width={400} height={300} alt="image"/>
+            <Image src='https://forcythe.com/images/Project%20Images/exec-pro.svg' width={400} height={300} alt="image"/>
+            <Image src='https://forcythe.com/images/Project%20Images/phone.svg' width={400} height={300} alt="image"/>
+            <Image src='https://forcythe.com/images/Project%20Images/starks.svg' width={400} height={300} alt="image"/>
+            <Image src='https://forcythe.com/images/Project%20Images/africaFund.svg' width={400} height={300} alt="image"/>
+        </div>
+        <div className="flex animate-marquee1">
+            <Image src='https://forcythe.com/images/Project%20Images/stac.svg' width={400} height={300} alt="image"/>
+            <Image src='https://forcythe.com/images/Project%20Images/activity.svg' width={400} height={300} alt="image"/>
+            <Image src='https://forcythe.com/images/Project%20Images/exec-pro.svg' width={400} height={300} alt="image"/>
+            <Image src='https://forcythe.com/images/Project%20Images/phone.svg' width={400} height={300} alt="image"/>
+            <Image src='https://forcythe.com/images/Project%20Images/starks.svg' width={400} height={300} alt="image"/>
+            <Image src='https://forcythe.com/images/Project%20Images/africaFund.svg' width={400} height={300} alt="image"/>
+        </div>
+        </div>
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,20 @@ export default {
       backgroundImage: {
         'header-background': "url('https://forcythe.com/images/header-background.svg')",
       },
-      
+      animation: {
+        marquee: "marquee 10s linear infinite",
+        marquee1: "marquee1 10s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+        marquee1: {
+          '0%': { transform: 'translateX(-50%)' },
+          '100%': { transform: 'translateX(0%)' },
+        }
+      }
     },
   },
   plugins: [],
